ae6e84028ac31edf2afa7f4b5df01b45a2bb9923
pulkban <pulkban@gmail.com>
2023-02-06 22:16:15 +0530

    Updated more shlokas

---
+++ lb.js
@@ -0,0 +1,73 @@
+journalDetails = {
+
+	'DhyanaShloka': 'lb/DhyanaShloka.html',
+	'AdityaHrudayam': 'lb/AdityaHrudayam.html',
+	'DurgaChandrakalaStuti': 'lb/DurgaChandrakalaStuti.html',
+	'GuruMantra': 'lb/GuruMantra.html',
+	'NavaratnaMalika': 'lb/NavaratnaMalika.html',
+	'NithyaParayanaShloka': 'lb/NithyaParayanaShloka.html',
+	'SammohanaKrishnaShlokam': 'lb/SammohanaKrishnaShlokam.html',
+	'ShantiMantra': 'lb/ShantiMantra.html',
+	'ShriSthuthi': 'lb/ShriSthuthi.html',
+	'ShriDurgaSapthashloki': 'ShriDurgaSapthashloki.html',
+	'ShreeRajaRajeswariAshtakam': 'ShreeRajaRajeswariAshtakam.html',
+	'MeenakshiPancharatnam': 'MeenakshiPancharatnam.html',
+	'ShreeKaamakshiStotram': 'ShreeKaamakshiStotram.html',
+	'NavaMangaliStotram': 'NavaMangaliStotram.html',
+	'ShreeAnnapoornaAshtakam': 'ShreeAnnapoornaAshtakam.html',
+	'ShreeLalithaPanchakam': 'ShreeLalithaPanchakam.html',
+	'ShardhaaBhujangaPryayathashtakam': 'ShardhaaBhujangaPryayathashtakam.html',
+};
+
+// keep a global reference to iframe as it will be needed multiple times
+myIframe = document.getElementById("my-iframe");
+
+function populateNavbar()
+{
+    // get reference to navbar
+    var myNavbar = document.getElementById("my-navbar");
+    for (var person in journalDetails)
+    {
+        // add a button for each person
+        // make it call main on click with event
+        myNavbar.innerHTML += `<a href='${journalDetails[person]}' onclick='showIframe(event)'>${person}</a><br>`
+    }
+}
+
+// we want to highlight which name was just clicked, to keep track of where in the index we currently are
+// to implement it we will have to clear this hightlight from previously clicked name
+// so keep last event handy to modify its highlight
+var lastEvent = null;
+
+function showIframe(currentEvent)
+{
+    // find title of button which was pressed
+    var person = currentEvent.target.textContent;
+
+    // clear highlight of last clicked name
+    if (lastEvent)
+    {
+        lastEvent.target.style.color = "#8ab4f8";
+        lastEvent.target.style.backgroundColor = "transparent";
+    }
+
+    // apply highlight to current clicked name
+    currentEvent.target.style.color = "black";
+    currentEvent.target.style.backgroundColor = "#8ab4f8";
+
+    // store current event as last event
+    lastEvent = currentEvent;
+    
+    // load iframe of this person's journal
+    myIframe.innerHTML = `<iframe src="${journalDetails[person]}" style="width: 100%; height: 100vh;"></iframe>`;
+
+    // prevent link from opening
+    currentEvent.preventDefault();
+}
+
+// populate navbar on pageload
+populateNavbar()
+
+// show first person's journal automatically
+document.getElementById("my-iframe").innerHTML = `<iframe src="${journalDetails['DhyanaShloka']}" style="width: 100%; height: 100vh;"></iframe>`;
+
