3213e1c7089fc971449196b160b93c9df8c4f457
pulkban <pulkban@gmail.com>
2023-02-12 13:33:21 +0530

    Some more in SD

---
+++ sd.js
@@ -0,0 +1,147 @@
+journalDetails = {
+
+	'DhyanaShloka': 'sd/DhyanaShloka.html',
+	'AdityaHrudayam': 'sd/AdityaHrudayam.html',
+	'DurgaChandrakalaStuti': 'sd/DurgaChandrakalaStuti.html',
+	'GuruMantra': 'sd/GuruMantra.html',
+	'NavaratnaMalika': 'sd/NavaratnaMalika.html',
+	'NithyaParayanaShloka': 'sd/NithyaParayanaShloka.html',
+	'SammohanaKrishnaShlokam': 'sd/SammohanaKrishnaShlokam.html',
+	'ShantiMantra': 'sd/ShantiMantra.html',
+	'ShriSthuthi': 'sd/ShriSthuthi.html',
+	'ShriDurgaSapthashloki': 'sd/ShriDurgaSapthashloki.html',
+	'ShreeRajaRajeswariAshtakam': 'sd/ShreeRajaRajeswariAshtakam.html',
+	'MeenakshiPancharatnam': 'sd/MeenakshiPancharatnam.html',
+	'ShreeKaamakshiStotram': 'sd/ShreeKaamakshiStotram.html',
+	'NavaMangaliStotram': 'sd/NavaMangaliStotram.html',
+	'ShreeAnnapoornaAshtakam': 'sd/ShreeAnnapoornaAshtakam.html',
+	'ShreeLalithaPanchakam': 'sd/ShreeLalithaPanchakam.html',
+	'ShardhaaBhujangaPryayathashtakam': 'sd/ShardhaaBhujangaPryayathashtakam.html',
+	'ArdhaNaareshwariStothram': 'sd/ArdhaNaareshwariStothram.html',
+	'BrahmaSamhita': 'sd/BrahmaSamhita.html',
+	'ChandrashekaramAshtakam': 'sd/ChandrashekaramAshtakam.html',
+	'ShriSwaminathaAshtakam': 'sd/ShriSwaminathaAshtakam.html',
+	'Thotakaashtakam': 'sd/Thotakaashtakam.html',
+	'NavagrahaStothram': 'sd/NavagrahaStothram.html',
+	'DeviKhadgamalaStothram': 'sd/DeviKhadgamalaStothram.html',
+	'DurgaPancharatnam': 'sd/DurgaPancharatnam.html',
+	'DurgaSahasranamaStothram': 'sd/DurgaSahasranamaStothram.html',
+	'GanaNayakaAshtakam': 'sd/GanaNayakaAshtakam.html',
+	'GaneshaBhujangam': 'sd/GaneshaBhujangam.html',
+	'IndrakshiStothram': 'sd/IndrakshiStothram.html',
+	'ShriDakshinaaMoorthyStothram': 'sd/ShriDakshinaaMoorthyStothram.html',
+	'ShriRamarStothram': 'sd/ShriRamarStothram.html',
+	'ShriKrishnaashtakam': 'sd/ShriKrishnaashtakam.html',
+	'MahalakshmiSthuthi': 'sd/MahalakshmiSthuthi.html',
+	'NavagrahaSthothra': 'sd/NavagrahaSthothra.html',
+	'ShriThulasiSthothram': 'sd/ShriThulasiSthothram.html',
+	'GuruvathaPureeshaPancharatnam': 'sd/GuruvathaPureeshaPancharatnam.html',
+	'ShriKamalaJadayitAshtakam': 'sd/ShriKamalaJadayitAshtakam.html',
+	'KanakadharaStothram': 'sd/KanakadharaStothram.html',
+	'LakshmiAshtotraSathanamaStotram': 'sd/LakshmiAshtotraSathanamaStotram.html',
+	'SriLakshmiSahasranamaStothram': 'sd/SriLakshmiSahasranamaStothram.html',
+	'SriLalithaNavarathnaMalai': 'sd/SriLalithaNavarathnaMalai.html',
+	'LalithaSahasranamam': 'sd/LalithaSahasranamam.html',
+	'LalithaStavaRatnam': 'sd/LalithaStavaRatnam.html',
+	'LalithaTrishati': 'sd/LalithaTrishati.html',
+	'MahishasuraMaridini': 'sd/MahishasuraMaridini.html',
+	'MantraMartuka': 'sd/MantraMartuka.html',
+	'MookaPanchaShathi': 'sd/MookaPanchaShathi.html',
+	'MukundaMaala': 'sd/MukundaMaala.html',
+	'NaamaRamayana': 'sd/NaamaRamayana.html',
+	'NatarajarStothram': 'sd/NatarajarStothram.html',
+	'PanchaayudhaStotram': 'sd/PanchaayudhaStotram.html',
+	'ShriRajarajeshwariNamaste': 'sd/ShriRajarajeshwariNamaste.html',
+	'SaraswathiSahasranamaStotram': 'sd/SaraswathiSahasranamaStotram.html',
+	'ShivaShadaksharaStotram': 'sd/ShivaShadaksharaStotram.html',
+	'ShivaThaandava': 'sd/ShivaThaandava.html',
+	'ShriVarahiKavacham': 'sd/ShriVarahiKavacham.html',
+	'ShyamalaDandakam': 'sd/ShyamalaDandakam.html',
+	'SivanandaLahari': 'sd/SivanandaLahari.html',
+	'ShivaStothram': 'sd/ShivaStothram.html',
+	'MahaMrtyunjayaMantra': 'sd/MahaMrtyunjayaMantra.html',
+	'Lingashtakam': 'sd/Lingashtakam.html',
+	'Bilwashtakam': 'sd/Bilwashtakam.html',
+	'ShivaPanchaaksharaStothram': 'sd/ShivaPanchaaksharaStothram.html',
+	'MargabandhuStothram': 'sd/MargabandhuStothram.html',
+	'ShriKaalabhairavaAshtakam': 'sd/ShriKaalabhairavaAshtakam.html',
+	'DhwaadhashaJyotirlingaSthothram': 'sd/DhwaadhashaJyotirlingaSthothram.html',
+	'VaidhyanaathaAshtakam': 'sd/VaidhyanaathaAshtakam.html',
+	'DhaaridryaDahanaShivastothram': 'sd/DhaaridryaDahanaShivastothram.html',
+	'ShriShivaAshtakam': 'sd/ShriShivaAshtakam.html',
+	'ShriShivaastotharaShatanamaStothram': 'sd/ShriShivaastotharaShatanamaStothram.html',
+	'ShriShivaMaanasaPooja': 'sd/ShriShivaMaanasaPooja.html',
+	'ShriShivaMangalashtakam': 'sd/ShriShivaMangalashtakam.html',
+	'ShriRudhrashtakam': 'sd/ShriRudhrashtakam.html',
+	'ShriShivaThaandavaStothram': 'sd/ShriShivaThaandavaStothram.html',
+	'SivaSivane': 'sd/SivaSivane.html',
+	'SoundaryaLahari': 'sd/SoundaryaLahari.html',
+	'SripadaSapthami': 'sd/SripadaSapthami.html',
+	'SriSubrahmanyaBhujangam': 'sd/SriSubrahmanyaBhujangam.html',
+	'SubramanyaSahasranamam': 'sd/SubramanyaSahasranamam.html',
+	'SriVenkateshwaraSuprabhatam': 'sd/SriVenkateshwaraSuprabhatam.html',
+	'SwayamvaraParvatiMantramaala': 'sd/SwayamvaraParvatiMantramaala.html',
+	'Thiruppavai': 'sd/Thiruppavai.html',
+	'Thiruvempavai': 'sd/Thiruvempavai.html',
+	'TripurasundariAshtakam': 'sd/TripurasundariAshtakam.html',
+	'Thirupalliyezhuchi': 'sd/Thirupalliyezhuchi.html',
+	'VaaraahiKavacham': 'sd/VaaraahiKavacham.html',
+	'VaaranamAayiram': 'sd/VaaranamAayiram.html',
+	'VelMaaral': 'sd/VelMaaral.html',
+	
+};
+
+
+
+// keep a global reference to iframe as it will be needed multiple times
+myIframe = document.getElementById("my-iframe");
+
+function populateNavbar()
+{
+    // get reference to navbar
+    var myNavbar = document.getElementById("my-navbar");
+    for (var person in journalDetails)
+    {
+        // add a button for each person
+        // make it call main on click with event
+        myNavbar.innerHTML += `<a href='${journalDetails[person]}' onclick='showIframe(event)'>${person}</a><br>`
+    }
+}
+
+// we want to highlight which name was just clicked, to keep track of where in the index we currently are
+// to implement it we will have to clear this hightlight from previously clicked name
+// so keep last event handy to modify its highlight
+var lastEvent = null;
+
+function showIframe(currentEvent)
+{
+    // find title of button which was pressed
+    var person = currentEvent.target.textContent;
+
+    // clear highlight of last clicked name
+    if (lastEvent)
+    {
+        lastEvent.target.style.color = "#8ab4f8";
+        lastEvent.target.style.backgroundColor = "transparent";
+    }
+
+    // apply highlight to current clicked name
+    currentEvent.target.style.color = "black";
+    currentEvent.target.style.backgroundColor = "#8ab4f8";
+
+    // store current event as last event
+    lastEvent = currentEvent;
+    
+    // load iframe of this person's journal
+    myIframe.innerHTML = `<iframe src="${journalDetails[person]}" style="width: 100%; height: 100vh;"></iframe>`;
+
+    // prevent link from opening
+    currentEvent.preventDefault();
+}
+
+// populate navbar on pageload
+populateNavbar()
+
+// show first person's journal automatically
+document.getElementById("my-iframe").innerHTML = `<iframe src="${journalDetails['DhyanaShloka']}" style="width: 100%; height: 100vh;"></iframe>`;
+
