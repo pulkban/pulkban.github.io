a802e309b8911d2a37fde4e864342ce455a5be4a
UdhaySap <UdhaySap>
2025-07-07 23:06:06 +0530

    pranev1

---
+++ exam/vup_maths.js
@@ -0,0 +1,226 @@
+window.getQuestions = function() {
+    return [
+
+
+
+
+{
+  "question": "{{images_vup_maths/vupmaths15.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "D",
+  "explanation": "The duck flew in two segments. We need to calculate the distance of each segment and then add them together<br>Segment 1: 18 km/hour for 3 hours Distance = Speed × Time = 18 km/hour × 3 hours = 54 km.<br>Segment 2: 15 km/hour for 2 hours Distance = Speed × Time = 15 km/hour × 2 hours = 30 km<br>Total Distance: 54 km + 30 km = 84 km"
+},
+{
+  "question": "{{images_vup_maths/vupmaths14.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "D",
+  "explanation": "Fraction to Decimal: 1 out of 8 is represented as the fraction 1/8. Divide 1 by 8 to get the decimal equivalent: 1 / 8 = 0.125.  Decimal to Percent: To convert a decimal to a percent, multiply by 100: 0.125 * 100 = 12.5%"
+},
+{
+  "question": "{{images_vup_maths/vupmaths13.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "B"
+},
+{
+  "question": "{{images_vup_maths/vupmaths12.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "B"
+},
+{
+  "question": "{{images_vup_maths/vupmaths11.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "C"
+},
+{
+  "question": "{{images_vup_maths/vupmaths10.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "B"
+},
+{
+  "question": "{{images_vup_maths/vupmaths9.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "C"
+},
+{
+  "question": "{{images_vup_maths/vupmaths8.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "A"
+},
+{
+  "question": "{{images_vup_maths/vupmaths7.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "D"
+},
+{
+  "question": "4 3/4 - 2 1/2 = ? {{images_vup_maths/vupmaths6.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "C"
+},
+{
+  "question": "Nina made a triangle by cutting the corner of a sheet of paper. One angle is 45°. What is the measure of the third angle of Nina's triangle?{{images_vup_maths/vupmaths5.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "30°",
+    "B": "45°",
+    "C": "55°",
+    "D": "60°"
+  },
+  "answer": "B"
+},
+{
+  "question": "Which of the following is best described in the given figure?{{images_vup_maths/vupmaths4.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "Acute angles",
+    "B": "Obtuse angles",
+    "C": "Parallel lines",
+    "D": "Perpendicular lines"
+  },
+  "answer": "D"
+},
+{
+  "question": "Which shows 833,000 written in scientific notation?",
+  "type": "single",
+  "options": {
+    "A": "8.33 x 10^3",
+    "B": "8.33 x 10^4",
+    "C": "8.33 x 10^5",
+    "D": "8.33 x 10^6"
+  },
+  "answer": "C"
+},
+{
+  "question": "Three of the following are alike in a certain way and form a group. Find the odd one out.",
+  "type": "single",
+  "options": {
+    "A": "Bird",
+    "B": "Insect",
+    "C": "Aeroplane",
+    "D": "Kite"
+  },
+  "answer": "B"
+},
+{
+  "question": "Insert the missing character. {{images_vup_maths/vupmaths2.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "1",
+    "B": "2",
+    "C": "3",
+    "D": "4"
+  },
+  "answer": "A",
+  "explanation": ""
+},
+{
+  "question": "Which most closely resembles the mirror image of the given word. STROKE{{images_vup_maths/vupmaths3.jpg}}",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "D"
+},
+{
+  "question": "The figure given below is made of 3 small cubes. {{images_vup_maths/vupmaths1.jpg}}Which best shows the side view of the figure?",
+  "type": "single",
+  "options": {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D"
+  },
+  "answer": "A"
+},
+{
+  "question": "Which will come next in the series? az, by, cx, ?",
+  "type": "single",
+  "options": {
+    "A": "ef",
+    "B": "gh",
+    "C": "ij",
+    "D": "dw"
+  },
+  "answer": "D"
+},
+{
+  "question": "A and B are a married couple. X and Y are brothers, X is brother of A. How is Y related to B?",
+  "type": "single",
+  "options": {
+    "A": "Brother-in-law",
+    "B": "Brother",
+    "C": "Son-in-law",
+    "D": "Cousin"
+  },
+  "answer": "A"
+}
+
+
+
+
+
+	
+];
+};
